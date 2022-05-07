7520168a55f7e59cd9939659daf3504bc9426e0f
Balazs Kelemen <kelbal@gmail.com>
2022-05-07 18:16:16 +0100

    feltolteni

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+ <div>
+<h1>Joke App</h1>
+<button @click="showJoke()">Show Joke</button>
+<div v-if="joke">
+<p>{{joke}}</p>
+</div>
+ </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+
+export default {
+  data(){
+    return{ 
+      joke:''
+    }
+  },
+  methods:{
+    async showJoke(){
+      let config ={
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+      const joke = await axios.get('https://icanhazdadjoke.com', config);
+      this.joke = joke.data.joke
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
